911fde55df2c27801462810037a0a256d5920a48
Abhimanyu Yadav <97447229+Melkor786@users.noreply.github.com>
2024-04-01 23:01:29 +0530

    Update AllPosts.jsx

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -17,7 +17,22 @@ function AllPosts() {
 
   // Filter posts written by the authenticated user
   const userPosts = posts.filter(post => post.userId === userData.$id);
-
+  
+  if (userPosts.length === 0) {
+    return (
+      <div className="w-full py-8 mt-4 text-center">
+        <Container>
+          <div className="flex flex-wrap">
+            <div className="p-2 w-full">
+              <h1 className="text-2xl font-bold hover:text-gray-500">
+                No posts
+              </h1>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
   return (
     <div className="w-full py-8">
       <Container>
