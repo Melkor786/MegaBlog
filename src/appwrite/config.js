6bcff2c0dfe7d0461abc5892b15c51838ebe1cb4
Abhimanyu Yadav <97447229+Melkor786@users.noreply.github.com>
2024-04-01 23:22:07 +0530

    Update config.js

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -81,12 +81,12 @@ export class Service{
         }
     }
 
-    async getPosts(queries = [Query.equal("status", "active")]){
+    async getPosts(/* queries = [Query.equal("status", "active")]*/){
         try {
             return await this.databases.listDocuments(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
-                queries,
+                /* queries,*/
                 
 
             )
